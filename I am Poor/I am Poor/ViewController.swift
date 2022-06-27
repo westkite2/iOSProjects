c85443056eb5a6da49098688df5fe3a5f0b2f7d1
westkite2 <westkite2@gmail.com>
2022-06-27 17:26:43 +0900

    Complete project - I am Poor

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Ayeon on 2022/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
